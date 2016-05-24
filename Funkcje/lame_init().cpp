df6618376e1fd604fd8ee70ef12bacd3f1fb90aa
pawel123qwe <pwegrzyn54@gmail.com>
2016-05-24 17:40:04 +0200

    Update lame_init().cpp

--- Funkcje/lame_init().cpp
+++ Funkcje/lame_init().cpp
@@ -3,12 +3,14 @@ lame_init(void)
     lame_global_flags *gfp;
     int     ret;
 
+    //Wywołanie jakiejś funkcji, musimy sprawdzić co jest jej zadaniem.  init_log_table();
     init_log_table();
-
+    
     gfp = calloc(1, sizeof(lame_global_flags));
     if (gfp == NULL)
         return NULL;
 
+    //Kolejna funkcja do przeanalizowania. lame_init_old(gfp);
     ret = lame_init_old(gfp);
     if (ret != 0) {
         free(gfp);
