6c11c39a2dda5060a39519f2404b6880576e16c6
pawel123qwe <pwegrzyn54@gmail.com>
2016-05-25 19:14:21 +0200

    Częściowa analiza lame_encode_flush()
    Napotkałem problem podczas analizy. Nie mogę wywnioskować czym jest
    zmienna end_padding. Spróbujcie Wy, może do czegoś dojdziemy ;-)

--- Funkcje/lame_encode_flush().cpp
+++ Funkcje/lame_encode_flush().cpp
@@ -17,38 +17,66 @@ lame_encode_flush(lame_global_flags * gfp, unsigned char *mp3buffer, int mp3buff
     int     is_resampling_necessary;
     double  resample_ratio = 1;
 
-    //Przeanalizować: is_lame_global_flags_valid(gfp)
+    //Przeanalizować: is_lame_global_flags_valid(gfp) oraz is_lame_internal_flags_valid(gfc)
+
+    //is_lame_global_flags_valid(gfp) - Wydaje mi sie że funckja ta sprawdza poprawność argumentu gfp który wysyłamy do funkcji.
+    //Jeśli jest on niepoprawny zwraca -3, wartość -3 zapewne jest po to, aby ew. kontrolować na jakim etapie mamy bład.
     if (!is_lame_global_flags_valid(gfp)) {
         return -3;
     }
+
+    //Jeśli gpf jest poprawce, wskaźnik gfc ustawiamy na składową gfp o naziwe internal_flags.
+    //Po raz kolejny mamy do czynienia z strukturą lame_global_flags. Musimy się dowiedzieć dokładnie co ona zawiera.
     gfc = gfp->internal_flags;
+
+    //Kolejna funkcja do sprawdzania poprawności argumentu ? To mi sugeruje nazwa, oraz przerwanie pracy w przypadku błedu ( return -3 )
     if (!is_lame_internal_flags_valid(gfc)) {
         return -3;
     }
+
+    //Odwołonie i przypisanie do nieznanych nam składowych. Kolejny szczegoł wymagający analizy.
     cfg = &gfc->cfg;
     esv = &gfc->sv_enc;
 
     /* Was flush already called? */
+
+    //Jeśli liczba 'próbek' do zdekodowania jest wartością niepoprawną, zwracamy 0;
     if (esv->mf_samples_to_encode < 1) {
         return 0;
     }
-    pcm_samples_per_frame = 576 * cfg->mode_gr;
-    mf_needed = calcNeeded(cfg);
-    //Przeanalizować: calcNeeded(cfg);
 
+    //Obliczenie liczby 'próbek' na jedną klatkę formatu pcm.
+    pcm_samples_per_frame = 576 * cfg->mode_gr;
+
+    //Przeanalizować: calcNeeded(cfg);
+    //Ta funkcja wylicza potrzebną wartość czegoś ( pytanie czego? ) i zapisuje ją do mf_needed.
+    //Trzeba będzie zajrzeć w ciało calcNeeded aby się przekonać co dokłądnie jest liczone.
+    mf_needed = calcNeeded(cfg);
+
+    //Wyliczenie liczby 'próbek' potrzebnych do zdekodowania.
     samples_to_encode = esv->mf_samples_to_encode - POSTDELAY;
 
     //Przeanalizować: memset(buffer, 0, sizeof(buffer));
+    //Przeoczona funkcja typu void:  memset(buffer, 0, sizeof(buffer)); - Również do analizy.
     memset(buffer, 0, sizeof(buffer));
     mp3count = 0;
 
     //Przeanalizować: isResamplingNecessary(cfg);
+    //Resampling – transformacja bitmapy polegająca na zmianie liczby jej pikseli.
+    //Efektem resamplingu jest rzeczywiste powiększenie, pomniejszenie lub zmiana proporcji, ale także i obrót obrazu bitmapowego.
+    //Tutaj sprawdzamy że resampling jest w naszym przypadku potrzebny, jeśli tak, to jest on wykonywany.
+    //Tutaj polega on na wyliczeniu stosunku częstotliwości próbkowania na wejściu, do częstotliwości na wyjściu.
+    //Oraz odpowiednim zmodyfikowaniu liczby 'próbek' koniecznych do zdekodowania.
     is_resampling_necessary = isResamplingNecessary(cfg);
     if (is_resampling_necessary) {
         resample_ratio = (double)cfg->samplerate_in / (double)cfg->samplerate_out;
         /* delay due to resampling; needs to be fixed, if resampling code gets changed */
         samples_to_encode += 16. / resample_ratio;
     }
+
+    //Czym jest end_padding ? Wg co znaczy słowo 'padding' - przyznam szczerze, że nie mam pojęcia ;-)
+    //Aby dalej kontunuować tą analizę, muszę to wiedzieć, gdyż widze że to dość znaczący parametr.
+
     end_padding = pcm_samples_per_frame - (samples_to_encode % pcm_samples_per_frame);
     if (end_padding < 576)
         end_padding += pcm_samples_per_frame;
@@ -99,7 +127,7 @@ lame_encode_flush(lame_global_flags * gfp, unsigned char *mp3buffer, int mp3buff
     /* mp3 related stuff.  bit buffer might still contain some mp3 data */
 
     //Przeanalizować: flush_bitstream(gfc);
-    //Przeanalizować: copy_buffer();
+    //Przeanalizować: copy_buffer() oraz save_gain_values(gfc);
 
     flush_bitstream(gfc);
     imp3 = copy_buffer(gfc, mp3buffer, mp3buffer_size_remaining, 1);
