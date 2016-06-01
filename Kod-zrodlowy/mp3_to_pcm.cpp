62a856fd338c3bca6a74dc106f439338acb15898
Koks1996 <maciejblaszczyk1996@gmail.com>
2016-06-01 19:09:35 +0200

    Zarzadzanie kodem

--- Kod-zrodlowy/mp3_to_pcm.cpp
+++ Kod-zrodlowy/mp3_to_pcm.cpp
@@ -6,42 +6,43 @@
 
 
 
-int pcm_to_mp3(char * pcm, char * mp3)
+int pcm_to_mp3 (char * pcm, char * mp3)
 {
     int read, write;
 
-    FILE *pcm = fopen("file.pcm", "rb");
+    FILE *pcm = fopen ("file.pcm", "rb");
 
     //Sprawdzam czy konwertowany plik .pcm istnieje, zeby nie naruszac pamieci gdy sie pomylimy przy wpisywaniu.
-	if(pcm == nullptr)
+	if (pcm == nullptr)
 		return -1;
 
     //Plik .mp3 jest tworzony automatycznie, jesli nie istnieje, wiec nie ma potrzeby sprawdzania.
-    FILE *mp3 = fopen("file.mp3", "wb");
+    FILE *mp3 = fopen ("file.mp3", "wb");
 
     const int PCM_SIZE = 8192;
     const int MP3_SIZE = 8192;
 
-    short int pcm_buffer[PCM_SIZE*2];
-    unsigned char mp3_buffer[MP3_SIZE];
+    short int pcm_buffer [PCM_SIZE * 2];
+    unsigned char mp3_buffer [MP3_SIZE];
 
-    lame_t lame = lame_init();
-    lame_set_in_samplerate(lame, 44100);
-    lame_set_VBR(lame, vbr_default);
-    lame_init_params(lame);
+    lame_t lame = lame_init ();
+    lame_set_in_samplerate (lame, 44100);
+    lame_set_VBR (lame, vbr_default);
+    lame_init_params (lame);
 
-    do {
-        read = fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
+    do 
+	{
+        read = fread (pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
         if (read == 0)
-            write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
+            write = lame_encode_flush (lame, mp3_buffer, MP3_SIZE);
         else
-            write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
-        fwrite(mp3_buffer, write, 1, mp3);
+            write = lame_encode_buffer_interleaved (lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
+        fwrite (mp3_buffer, write, 1, mp3);
     } while (read != 0);
 
-    lame_close(lame);
-    fclose(mp3);
-    fclose(pcm);
+    lame_close (lame);
+    fclose (mp3);
+    fclose (pcm);
 
     return 0;
 }
